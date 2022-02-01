69c7f734f0867a8c837de0aa9cf5786e7a4ce4f0
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-02-01 15:06:16 -0300

    Criando a tela de links e tornando ela responsiva e adicionando alguns efeitos

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,7 +1,45 @@
+import "./styles.css";
+import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 function Links() {
     return (
-      <div>
-        <h1>Página Links</h1>
+      <div className="links-container">
+
+          <div className="links-header">
+            <Link to={"/"}>
+              <FiArrowLeft size={38} color="#FFF"/>
+            </Link>
+            <h1>Meus Links</h1>
+
+          </div>
+
+          <div className="links-item">
+
+            <button className="link">
+              <FiLink size={18} color="#FFF"/>
+              https://ricardosanatana.github.com
+            </button>
+
+            <button className="link-delete">
+              <FiTrash size={24} color="#FF5454"/>
+            </button>
+
+          </div>
+
+
+          <div className="links-item">
+
+            <button className="link">
+              <FiLink size={18} color="#FFF"/>
+              https://ricardosanatana.github.com
+            </button>
+
+            <button className="link-delete">
+              <FiTrash size={24} color="#FF5454"/>
+            </button>
+
+          </div>
       </div>
     );
   }
